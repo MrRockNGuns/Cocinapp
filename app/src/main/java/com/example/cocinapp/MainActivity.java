a3b73aac749d8d02f2fd626b790ebcbb36fcac04
Boris <mrrockboris@gmail.com>
2021-04-05 17:09:50 -0300

    "Add Activity Login and Forgot Password"

---
+++ app/src/main/java/com/example/cocinapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cocinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE); // esto Permite no mostrar la barra la primera vez
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ForgotPassword(View view){
+        Intent myIntent = new Intent(MainActivity.this, ForgotPassword.class);
+        startActivity(myIntent);
+
+    }
+}
