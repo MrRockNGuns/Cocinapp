6adf5300e4fb22c6b5293f1fe9fcc8e52d40fe5b
Boris <mrrockboris@gmail.com>
2021-04-13 21:00:39 -0300

    Fusion de los 2 repositorios

---
+++ app/src/main/java/com/example/cocinapp/full_recipe.java
@@ -0,0 +1,14 @@
+package com.example.cocinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class full_recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_recipe);
+    }
+}
