020df2e920a226a589438e03663b7a5c652d4cee
Boris <mrrockboris@gmail.com>
2021-04-24 17:44:57 -0300

    Agregado el menu debajo y voy a preparar el webview

--- app/src/main/java/com/example/cocinapp/MainActivity.java
+++ app/src/main/java/com/example/cocinapp/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.cocinapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnLogin;
+    private BottomNavigationView miNavegacion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE); // esto Permite no mostrar la barra la primera vez
+        //supportRequestWindowFeature(Window.FEATURE_NO_TITLE); // esto Permite no mostrar la barra la primera vez
         setContentView(R.layout.activity_main);
 
+        miNavegacion = (BottomNavigationView) findViewById(R.id.bottom_menu);
+
+        miNavegacion.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+
+                return true;
+            }
+        });
     }
 
     public void ForgotPassword(View view){
@@ -25,4 +41,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(myIntent);
 
     }
+
+
 }
